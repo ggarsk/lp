537b968b28108994723386d00d22e58ee304307a
Gabriel Pontes <gabriel.hgarsk@gmail.com>
2024-06-29 15:59:30 -0300

    Construindo uma função de busca linear em js. Parâmetros de função

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello, world!')
+
+let num = [10, 12, 16, 18, 22]
+
+function buscaLinear(num, valorBuscado) {
+    for (let numero of numeros) {
+        if (numero === valorBuscado) {
+            return true
+        }
+    }
+    return false
+}
+
+if (buscaLinear, 16) {
+    console.log('Valor encontrado!')
+} else {
+    console.log('O valor não foi encontrado. Onde está esse valor??')
+}
