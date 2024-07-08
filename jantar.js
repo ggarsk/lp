62341a0269c83608991225a5f452769c830bc9b2
Gabriel Pontes <gabriel.hgarsk@gmail.com>
2024-07-08 19:41:17 -0300

    Explicação comentada da lógica por trás do exercício jantar.js

--- jantar.js
+++ jantar.js
@@ -16,10 +16,10 @@ let restauranteAberto = prompt('Seu restaurante favorito está aberto? (sim/não
 // }
 
 // RESOLUÇÃO QUE CHEGUEI AO PENSAR MELHOR
-if (fome === "não" || dindin === 'não'){
+if (fome === "não" || dindin === 'não'){ // Se não tiver fome OU não tiver dinheiro, a janta será em casa
     console.log('Parece que a janta vai ser em casa hoje :(')
-} else if(dindin === 'sim' && restauranteAberto === 'sim'){
+} else if(dindin === 'sim' && restauranteAberto === 'sim'){ // A linha 19 já verifica se tem fome ou não. Se tiver, entra neste bloco. Agora, se tiver dinheiro e o restaurante estiver aberto, a janta será no restaurante favorito
     console.log('Hoje a janta vai ser no seu restaurante favorito!')
-} else {
+} else { // Se tiver dinheiro mas o restaurante estiver fechado, peça um deliver OU se não tiver dinheiro para o restaurante, peça um delivery
     console.log('Peça um delivery!')
 }
