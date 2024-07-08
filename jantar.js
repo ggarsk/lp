57f2deaa61b5e89aac4ef5676127914c0b81f7b7
Gabriel Pontes <gabriel.hgarsk@gmail.com>
2024-07-08 19:38:38 -0300

    Correção e teste do exercício jantar.js

--- jantar.js
+++ jantar.js
@@ -1,27 +1,25 @@
 // Script de exercício para colocar em prática IF/ELSE e operadores lógicos E e OU
 
-let fome = prompt('Ei! Você está com fome?')
-let dindin = prompt('E você tem dinheiro?')
-let restauranteAberto = prompt('Seu restaurante favorito está aberto?')
+let fome = prompt('Ei! Você está com fome? (sim/não)')
+let dindin = prompt('E você tem dinheiro? (sim/não)')
+let restauranteAberto = prompt('Seu restaurante favorito está aberto? (sim/não)')
 
-if(fome === 'não'){
-    console.log('Parece que a janta vai ser em casa hoje :(')
-} else if(dindin === 'não' || restauranteAberto === 'não'){
+// PRIMEIRA RESOLUÇÃO EM QUE CHEGUEI
+// if(fome === 'não'){
+//     console.log('Parece que a janta vai ser em casa hoje :(')
+// } else if(dindin === 'não' || restauranteAberto === 'não'){
+//     console.log('Parece que a janta vai ser em casa hoje :(')
+// } else if(dindin === 'sim' && restauranteAberto === 'sim'){
+//     console.log('Hoje a janta vai ser no seu restaurante favorito!')
+// } else if(dindin === 'sim' && restauranteAberto === 'não'){
+//     console.log('Peça um delivery!')
+// }
+
+// RESOLUÇÃO QUE CHEGUEI AO PENSAR MELHOR
+if (fome === "não" || dindin === 'não'){
     console.log('Parece que a janta vai ser em casa hoje :(')
 } else if(dindin === 'sim' && restauranteAberto === 'sim'){
     console.log('Hoje a janta vai ser no seu restaurante favorito!')
+} else {
+    console.log('Peça um delivery!')
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
